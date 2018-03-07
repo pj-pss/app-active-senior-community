fcaabbd5a9225d446b8caee400d7d430b1e280d6
Tsukahara <tsukahara.takut@jp.fujitsu.com>
2018-03-07 16:25:16 +0900

    fix app url

--- src/html/js/constant.js
+++ src/html/js/constant.js
@@ -1,13 +1,13 @@
 const TYPE = {
     INFO: 0,
     EVENT: 1
-}
+};
 
 const SEX = {
     ALL: 0,
     MALE: 1,
     FEMALE: 2
-}
+};
 
 const AGE = {
     ALL: 0,
@@ -15,18 +15,18 @@ const AGE = {
     SEVENTY: 2,
     SIXTY: 3,
     UNDER_FIFTY: 4
-}
+};
 
 const REPLY = {
     CONSIDER: 0,
     JOIN: 1
-}
+};
 
 Object.freeze(TYPE);
 Object.freeze(SEX);
 Object.freeze(AGE);
 Object.freeze(REPLY);
 
-const APP_URL = "https://demo.personium.io/app-fst-community-user/";
-const APP_BOX_NAME = 'io_personium_demo_app-fst-community-user';
-const ORGANIZATION_CELL_URL = 'https://demo.personium.io/fst-community-organization/'
+const APP_URL = "https://demo.personium.io/app-life-enrichers-community/";
+const APP_BOX_NAME = 'io_personium_demo_app-life-enrichers-community';
+const ORGANIZATION_CELL_URL = 'https://demo.personium.io/fst-community-organization/';
