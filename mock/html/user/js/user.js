829b9ad9c4b9ff201031aaede143dcaeb57fb8a8
Tsukahara <tsukahara.takut@jp.fujitsu.com>
2018-03-20 15:46:05 +0900

    Revert deleted mock source.

--- mock/html/user/js/user.js
+++ mock/html/user/js/user.js
@@ -838,6 +838,9 @@ $(function() {
     $("#articleComment").load("articleComment.html", function(){
         $("#modal-comment").load("modal-comment.html");
     });
+    $("#eventList").load("eventList.html");
+    $("#infoList").load("infoList.html");
+    $("#eventHistoryList").load("eventHistoryList.html");
 
     $("#modal-nfcReader").load("modal-nfcReader.html");
     $("#modal-helpConfirm").load("modal-helpConfirm.html");
